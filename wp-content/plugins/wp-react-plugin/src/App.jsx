35104193e1745411bef8c506a51e9faed1ab4ea1
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-07-27 14:55:58 +0300

    usage of Counter button component

--- wp-content/plugins/wp-react-plugin/src/App.jsx
+++ wp-content/plugins/wp-react-plugin/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import CounterButton from './components/CounterButton.jsx';
 
 export default function App() {
 	const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ export default function App() {
 		<div className="p-4">
 			<h2 style={{ margin: 0 }}>Hello from React inside WordPress 👋</h2>
 			<p>REST URL from PHP: <code>{window.WP_REACT_PLUGIN?.restUrl}</code></p>
-			<button onClick={() => setCount(c => c + 1)}>Clicked {count} times</button>
+			<CounterButton count={count} onClick={()=>setCount(c=>c+1)} />
 
 			<p style={{ opacity: .7, fontSize: 12, marginTop: 8 }}>
 				v{window.WP_REACT_PLUGIN?.version || 'dev'}
