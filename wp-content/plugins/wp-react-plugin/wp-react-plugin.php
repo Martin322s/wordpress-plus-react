65362f729ac9fbb3dd46d88821952c96f8aafcc9
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-08-08 17:30:02 +0300

    set admin access to the plugin

--- wp-content/plugins/wp-react-plugin/wp-react-plugin.php
+++ wp-content/plugins/wp-react-plugin/wp-react-plugin.php
@@ -32,7 +32,26 @@ class WP_React_Plugin_Starter {
         echo '<div class="wrap"><h1>React Demo</h1><div id="wp-react-root"></div></div>';
     }
 
-    
+    public function enqueue_admin_assets($hook) {
+        if ($hook !== 'toplevel_page_' . self::SLUG) return;
+
+        $base = plugin_dir_url(__FILE__);
+        $path = plugin_dir_path(__FILE__);
+
+        $js  = $base . 'build/index.js';
+        $css = $base . 'build/index.css';
+
+        $ver_js  = file_exists($path.'build/index.js')  ? filemtime($path.'build/index.js')  : '1.0.0';
+        $ver_css = file_exists($path.'build/index.css') ? filemtime($path.'build/index.css') : '1.0.0';
+
+        wp_enqueue_style('wp-react-plugin', $css, [], $ver_css);
+        wp_enqueue_script('wp-react-plugin', $js, ['wp-element'], $ver_js, true);
+
+        wp_localize_script('wp-react-plugin', 'WP_REACT_PLUGIN', [
+            'restUrl' => esc_url_raw( rest_url() ),
+            'nonce'   => wp_create_nonce('wp_rest')
+        ]);
+    }
 }
 
 new WP_React_Plugin_Starter();
